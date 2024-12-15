06572825bb59cbd6f6439b28bf359bb67f7dd0cc
@hexlet/cli <support@hexlet.io>
2024-12-15 22:58:11 +0300

    submit java-spring-ru/jpa-data

--- java-spring-ru/jpa-data/src/main/java/exercise/model/Person.java
+++ java-spring-ru/jpa-data/src/main/java/exercise/model/Person.java
@@ -1,9 +1,13 @@
 package exercise.model;
 
-import jakarta.persistence.*;
+
 
 import static jakarta.persistence.GenerationType.IDENTITY;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.GeneratedValue;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;

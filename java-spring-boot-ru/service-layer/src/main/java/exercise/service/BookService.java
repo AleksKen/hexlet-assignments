524400be60b0d06f13ab62d800beb5d24e870bb5
@hexlet/cli <support@hexlet.io>
2024-12-24 11:06:50 +0300

    submit java-spring-boot-ru/service-layer

--- java-spring-boot-ru/service-layer/src/main/java/exercise/service/BookService.java
+++ java-spring-boot-ru/service-layer/src/main/java/exercise/service/BookService.java
@@ -5,7 +5,9 @@ import exercise.dto.BookDTO;
 import exercise.dto.BookUpdateDTO;
 import exercise.exception.ResourceNotFoundException;
 import exercise.mapper.BookMapper;
+import exercise.repository.AuthorRepository;
 import exercise.repository.BookRepository;
+import jakarta.validation.ConstraintViolationException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -20,6 +22,9 @@ public class BookService {
     @Autowired
     private BookMapper bookMapper;
 
+    @Autowired
+    private AuthorRepository authorRepository;
+
     public List<BookDTO> getAll() {
         return bookRepository.findAll().stream().map(bookMapper::map).toList();
     }
@@ -32,6 +37,9 @@ public class BookService {
 
     public BookDTO create(BookCreateDTO dto) {
         var book = bookMapper.map(dto);
+        if (!authorRepository.existsById(dto.getAuthorId())) {
+            throw new ConstraintViolationException("Author with id " + dto.getAuthorId() + " not found!", null);
+        }
         bookRepository.save(book);
         return bookMapper.map(book);
     }
@@ -39,6 +47,11 @@ public class BookService {
     public BookDTO update(BookUpdateDTO dto, Long id) {
         var book = bookRepository.findById(id)
                 .orElseThrow(() -> new ResourceNotFoundException("Book with id " + id + " not found!"));
+
+        if (!authorRepository.existsById(dto.getAuthorId().get())) {
+            throw new ConstraintViolationException("Author with id " + dto.getAuthorId() + " not found!", null);
+        }
+
         bookMapper.update(dto, book);
         bookRepository.save(book);
         return bookMapper.map(book);
